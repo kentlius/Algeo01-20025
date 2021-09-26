88fade976af49282cf7cec71cdd7797505c5118f
fdavin <88297362+fdavin@users.noreply.github.com>
2021-09-26 20:09:19 +0700

    tambah bacamatriksfile driver

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
 public class Driver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         int pilihan = 0;
         int pilihanspl = 0;
+        String filename = "";
+        
         do{
             System.out.print("MENU");
             System.out.print("\n1. Sistem Persamaan Linier");
@@ -25,20 +27,31 @@ public class Driver {
                 pilihanspl = input.nextInt();
                 if (pilihanspl==1)
                 {
-                    System.out.print("Masukkan baris: ");
+                    System.out.print("\nMasukkan baris: ");
                     int baris1 = input.nextInt();
                     System.out.print("Masukkan kolom: ");
                     int kolom1 = input.nextInt();
                     Matriks m1 = new Matriks(baris1, kolom1);
 
-                    System.out.println("Isi matriks:");
-                    m1.bacaMatriks();
+                    System.out.println("Membaca text(1) atau input(2) : ");
+                    int pembaca = input.nextInt();
+                    if(pembaca == 2){
+                        System.out.println("Isi matriks:");
+                        m1.bacaMatriks();
+                    }
+                    else if(pembaca == 1){
+                        System.out.println("Nama text: ");
+                        filename = input.next();
+                        m1.bacaMatriksText(filename, baris1, kolom1);
+
+                    }
                     m1.splGauss();
                     
                     System.out.println("Hasil matriks:");
                     m1.tulisMatriks();
 
                     System.out.println(m1.solveGauss());
+                    System.out.print("\n");
                 }
                 if(pilihanspl==2)
                 {
